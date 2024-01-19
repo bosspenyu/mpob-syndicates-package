21b7fb561ef100b63df7428b78fd80b5b97b03f9
Ahmad Muhaimi Mohamad <muhaimi.mohamad@mdec.com.my>
2024-01-20 02:28:22 +0800

    database 2

--- config/config.php
+++ config/config.php
@@ -4,25 +4,23 @@
  * You can place your custom package configuration in here.
  */
 return [
-    'connections' => [
-        'syndicates' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_SYNDICATES_URL'),
-            'host' => env('DB_SYNDICATES_HOST', '127.0.0.1'),
-            'port' => env('DB_SYNDICATES_PORT', '3306'),
-            'database' => env('DB_SYNDICATES_DATABASE', 'forge'),
-            'username' => env('DB_SYNDICATES_USERNAME', 'forge'),
-            'password' => env('DB_SYNDICATES_PASSWORD', ''),
-            'unix_socket' => env('DB_SYNDICATES_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-    ]
+    'syndicates' => [
+        'driver' => 'mysql',
+        'url' => env('DATABASE_SYNDICATES_URL'),
+        'host' => env('DB_SYNDICATES_HOST', '127.0.0.1'),
+        'port' => env('DB_SYNDICATES_PORT', '3306'),
+        'database' => env('DB_SYNDICATES_DATABASE', 'forge'),
+        'username' => env('DB_SYNDICATES_USERNAME', 'forge'),
+        'password' => env('DB_SYNDICATES_PASSWORD', ''),
+        'unix_socket' => env('DB_SYNDICATES_SOCKET', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'strict' => true,
+        'engine' => null,
+        'options' => extension_loaded('pdo_mysql') ? array_filter([
+            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        ]) : [],
+    ],
 ];
