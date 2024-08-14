96135d3d186c2e38943c00d8fc390fef3b649293
bosspenyu <ahmad.muhaimi.mohamad@gmail.com>
2024-08-14 10:50:22 +0800

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    protected $table = 'USR';
+    protected $table = 'users';
     protected $appends = ['region','auth_role'];
 
     /**
